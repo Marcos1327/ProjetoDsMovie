bf914ba0b28b3bd4683445427fbddb44d6ec04cf
Marcos Giovanny <giovannygigi@gmail.com>
2022-01-13 16:31:53 -0300

    Save Score

---
+++ backend/src/main/java/com/devsuperior/dsmovie/repository/ScoreRepository.java
@@ -0,0 +1,14 @@
+package com.devsuperior.dsmovie.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devsuperior.dsmovie.entities.Score;
+import com.devsuperior.dsmovie.entities.ScorePK;
+
+@Repository
+public interface ScoreRepository extends JpaRepository<Score, ScorePK> {
+	
+	
+
+}
